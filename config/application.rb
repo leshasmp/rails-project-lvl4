202c7f85cc92ed935dab6ff1dcba261a9c28b008
alexey <alexey-melekhov1989@yandex.ru>
2022-05-30 16:13:35 +0500

    change rollbar version

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GithubQuality
+module BulletinBoard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -18,5 +20,6 @@ module GithubQuality
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ru
   end
 end
