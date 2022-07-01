dd0d125f955f055e9715ce56a881d4376fc41cc2
alexey <alexey-melekhov1989@yandex.ru>
2022-07-01 20:16:05 +0500

    add api check test

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -6,7 +6,6 @@ class Api::ChecksController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    params = JSON.parse(request.raw_post)
     @repository = Repository.find_by github_id: params['repository']['id']
     @check = @repository.checks.build
     if @check.save
---
+++ test/controllers/api/checks_controller_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @repository = repositories :one
+  end
+
+  test 'should create check' do
+    post api_checks_url params: { repository: { id: @repository.github_id } }
+
+    check = Repository::Check.find_by repository_id: @repository.id
+    assert { check }
+  end
+end
