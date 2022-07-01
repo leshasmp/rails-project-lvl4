334c8c6220dd9860ff6e2c7c89e8dd1b0b438365
alexey <alexey-melekhov1989@yandex.ru>
2022-07-01 20:23:10 +0500

    delete redirect for api check

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -8,11 +8,7 @@ class Api::ChecksController < Api::ApplicationController
   def create
     @repository = Repository.find_by github_id: params['repository']['id']
     @check = @repository.checks.build
-    if @check.save
-      redirect_to repository_path(@repository), notice: t('.success')
-      CheckRepositoryLoaderJob.perform_later @check.id
-    else
-      redirect_to repository_path(@repository), flash: { error: t('.error') }
-    end
+
+    CheckRepositoryLoaderJob.perform_later @check.id if @check.save
   end
 end
