8e8540f9680124773c0245aa44f8e1d2adfdcbba
alexey <alexey-melekhov1989@yandex.ru>
2022-07-02 15:25:15 +0500

    change config for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "github_quality_production"
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST', nil) }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -79,7 +80,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
