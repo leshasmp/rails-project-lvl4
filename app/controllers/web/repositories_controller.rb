acf90ef2d257644ad5ea9399e317273fab101481
alexey <alexey-melekhov1989@yandex.ru>
2022-08-06 16:30:24 +0500

    fix select repo error, add rubocop for repo

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -2,53 +2,60 @@
 
 require 'octokit'
 
-class Web::RepositoriesController < Web::ApplicationController
-  after_action :verify_authorized
+module Web
+  class RepositoriesController < Web::ApplicationController
+    after_action :verify_authorized
 
-  def index
-    authorize Repository
-    @repositories = current_user.repositories.all.order('created_at DESC')
-  end
-
-  def new
-    authorize Repository
-    @repository = Repository.new
-
-    @user_repositories = []
-    language_values = Repository.language.values
-
-    client = RepositoryInfo.new token: current_user.token
-    filtered_repos = client.repos.filter do |repos|
-      language = repos['language'].downcase if repos['language'].present?
-      language_values.include? language
+    def index
+      authorize Repository
+      @repositories = current_user.repositories.all.order('created_at DESC')
     end
 
-    filtered_repos.each do |repos|
-      @user_repositories << [repos['full_name'], repos['id']]
+    def new
+      authorize Repository
+      @repository = Repository.new
+      @user_repositories = user_repositories
     end
-  end
 
-  def create
-    authorize Repository
-    @repository = current_user.repositories.find_or_initialize_by(permitted_params)
+    def create
+      authorize Repository
+      @repository = current_user.repositories.find_or_initialize_by(permitted_params)
 
-    if @repository.save
-      redirect_to repositories_path, notice: t('.success')
-      RepositoryLoaderJob.perform_later @repository.id
-    else
-      render :new, status: :unprocessable_entity
+      if @repository.save
+        redirect_to repositories_path, notice: t('.success')
+        RepositoryLoaderJob.perform_later @repository.id
+      else
+        @user_repositories = user_repositories
+        render :new, status: :unprocessable_entity
+      end
     end
-  end
 
-  def show
-    @repository = Repository.find(params[:id])
-    @checks = @repository.checks.order('created_at DESC').page(params[:page])
-    authorize @repository
-  end
+    def show
+      @repository = Repository.find(params[:id])
+      @checks = @repository.checks.order('created_at DESC').page(params[:page])
+      authorize @repository
+    end
 
-  private
+    private
 
-  def permitted_params
-    params.require(:repository).permit(:github_id)
+    def permitted_params
+      params.require(:repository).permit(:github_id)
+    end
+
+    def user_repositories
+      result = []
+      language_values = Repository.language.values
+
+      client = RepositoryInfo.new token: current_user.token
+      filtered_repos = client.repos.filter do |repos|
+        language = repos['language'].downcase if repos['language'].present?
+        language_values.include? language
+      end
+
+      filtered_repos.each do |repos|
+        result << [repos['full_name'], repos['id']]
+      end
+      result
+    end
   end
 end
--- app/lib/repository_check_api.rb
+++ app/lib/repository_check_api.rb
@@ -17,7 +17,9 @@ class RepositoryCheckApi
   def self.run_check(lang, name, clone_url)
     command = command_check(lang)
     path = repo_path(name)
-    stdout, _exit_status = Open3.popen3("rm -rf #{path} && git clone #{clone_url} #{path} && #{command} #{path}") do |_stdin, stdout, _stderr, wait_thr|
+    stdout, _exit_status = Open3.popen3("rm -rf #{path} &&
+      git clone #{clone_url} #{path} &&
+       #{command} #{path} --config .rubocop_repos.yml") do |_stdin, stdout, _stderr, wait_thr|
       [stdout.read, wait_thr.value]
     end
     stdout
